038cfc2439b16700fc8ecee479fb5949afcc4e4a
Tandrac <tommy1196@gmail.com>
2018-11-28 21:14:03 -0500

    Making sure commits work

---
+++ RSA/rsa.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 20:56:52 2018
+
+@author: Tommy
+"""
+
+import math
+
+def coprime(x, y):
+    return math.gcd(a, b) == 1
+
+def main():
+    p = rand prime
+    q = rand prime
+    n = p*q
+    totient = (p-1)*(q-1)
+    return coprime(3,7)
+    
+    
+if __name__ == "__main__":
+    main()
