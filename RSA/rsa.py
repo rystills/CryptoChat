de9956bb8711d6f32a67c1b1d8e74bfccd3c630f
Tandrac <tommy1196@gmail.com>
2018-12-02 17:19:49 -0500

    RSA should be done

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -6,6 +6,7 @@ Created on Wed Nov 28 20:56:52 2018
 """
 
 import math, random
+import primality
 
 def coprime(x, y):
     return math.gcd(x, y) == 1
@@ -13,14 +14,14 @@ def coprime(x, y):
 def lcm(x,y):
     lcm = (x*y)//math.gcd(x,y)
     return lcm
-    
+
 # x = mulinv(b) mod n, (x * b) % n == 1
 def mulinv(e, lam):
     g, x, _ = extended_euclid(lam, e)
     if g == 1:
         return x % lam
 
-        
+
 def extended_euclid(totient,e):
     x0 = 1
     x1 = 0
@@ -32,47 +33,53 @@ def extended_euclid(totient,e):
         r = totient - q*e
         x = x1 - q*x0
         y = y1 - q*y0
-        
+
         totient = e
         e = r
         x1 = x0
         x0 = x
         y1 = y0
         y0 = y
-        
+
     d = totient
     x = x1
     y = y1
     return d, x, y
 
-def main():
-    p = 61
-    q = 53
+#Does RSA, and returns the public key pair (n,e) and private key pair (n,d)
+#As a pair themselves (pulbic_key, private_key)
+def RSA():
+    #P and Q are large primes
+    p = primality.generatePrime()
+    q = primality.generatePrime()
     n = p*q
-    #totient = (p-1)*(q-1)
+
+    #Carmihcaels totient
     lcm2 = lcm(p-1,q-1)
-    
-    #e must be coprime to totient, and using iteration to find totient to easy
-    #to break
+
+    #e must be coprime to totient, and using iteration to find totient to easy to break
     e = random.randrange(2, lcm2)
     testCo = coprime(e, lcm2)
-    
-    
-    
+
     while testCo != True:
         #changed 1 to 2 so cant use 1 as the coprime
         e = random.randrange(2, lcm2)
-        testCo = coprime(e, lcm2) 
-    
+        testCo = coprime(e, lcm2)
+
     #hardcoded to test examples
-    e = 17
-      
+    #e = 17
+
     d = mulinv(e,lcm2)
 
     public_key = (n, e)
-    private_key = (n, d)    
+    private_key = (n, d)
     print("P=",p," Q=",q," n=",n," e=",e," lcm=",lcm2," d=",d)
     print("Public key is :", public_key)
     print("Private key is:", private_key)
+    return(public_key, private_key)
+
+def main():
+    RSA()
+
 if __name__ == "__main__":
     main()
